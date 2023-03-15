dba78f463e0d13ef565fa4a4fb77c706574c8f40
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2023-03-15 16:52:30 -0400

    sideNav

--- frontend/src/components/SideNav.js
+++ frontend/src/components/SideNav.js
@@ -1,218 +1,66 @@
 import { useEffect, useRef, useState } from "react";
 import { BiChevronDown, BiChevronUp } from "react-icons/bi";
-import { MdLocationOn } from "react-icons/md";
-import {
-  carMakes,
-  bodyStyleData,
-  carEngineTypes,
-  carFuelTypes,
-  carTransmissionTypes,
-  drivetrains,
-  bodyColors,
-  interiorColors,
-  otherOptions,
-  yearsArray,
-} from "../assets/base/data";
-
-const sideNavData = [
-  {
-    id: 1,
-    title: "Location",
-    submenu: {
-      title: "distacnce with in",
-      min: 0,
-      max: 1500,
-    },
-    component: "range",
-    label: "distance",
-  },
-  {
-    id: 2,
-    title: "Selected Make",
-    submenu: carMakes,
-    component: "select",
-    label: "Make",
-  },
-  {
-    id: 3,
-    title: "Year",
-    submenu: yearsArray,
-    component: "select",
-    label: "select year",
-  },
-  {
-    id: 4,
-    title: "Price",
-    submenu: {},
-    component: "range",
-    label: "max price",
-  },
-  {
-    id: 5,
-    title: "Milage",
-    submenu: {},
-    component: "range",
-    label: "max mileage",
-  },
-  {
-    id: 6,
-    title: "Body Style",
-    submenu: bodyStyleData,
-    component: "checkbox",
-    label: "choose body style",
-  },
-  {
-    id: 7,
-    title: "Engine type",
-    submenu: carEngineTypes,
-    component: "checkbox",
-    label: "Engine",
-  },
-  {
-    id: 8,
-    title: "Fule Type",
-    submenu: carFuelTypes,
-    component: "checkbox",
-    label: "Fuel Type",
-  },
-  {
-    id: 9,
-    title: "Transmission",
-    submenu: carTransmissionTypes,
-    component: "checkbox",
-    label: "Transmission",
-  },
-  {
-    id: 10,
-    title: "Drivetrain",
-    submenu: drivetrains,
-    component: "checkbox",
-    label: "Drivetrain",
-  },
-  {
-    id: 11,
-    title: "Body Color",
-    submenu: bodyColors,
-    component: "checkbox",
-    label: "Body Color",
-  },
-  {
-    id: 12,
-    title: "Interior Color",
-    submenu: interiorColors,
-    component: "checkbox",
-    label: "Interior Color",
-  },
-  {
-    id: 13,
-    title: "Other Options",
-    submenu: otherOptions,
-    component: "checkbox",
-    label: "Other Options",
-  },
-];
+import { initialSearchState, sideNavData } from "../assets/base/data";
 
 const initialToggleState = Array(sideNavData.length).fill(false);
 
-const initialSearchState = [
-  {
-    id: 1,
-    value: 100,
-    title: "Location",
-  },
-  {
-    id: 2,
-    value: "All",
-    title: "Selected Make",
-  },
-  {
-    id: 3,
-    value: 2000,
-    title: "Year",
-  },
-  {
-    id: 4,
-    value: "Any Price",
-    title: "Price",
-  },
-  {
-    id: 5,
-    value: "Any Mileage",
-    title: "Milage",
-  },
-  {
-    id: 6,
-    value: "Any Style",
-    checked: false,
-    title: "Body Style",
-  },
-  {
-    id: 7,
-    value: "",
-    checked: false,
-    title: "Engine type",
-  },
-  {
-    id: 8,
-    value: "",
-    checked: false,
-    title: "Fule Type",
-  },
-  {
-    id: 9,
-    value: "",
-    checked: false,
-    title: "Transmission",
-  },
-  {
-    id: 10,
-    value: "",
-    checked: false,
-    title: "Drivetrain",
-  },
-  {
-    id: 11,
-    value: "",
-    checked: false,
-    title: "Body Color",
-  },
-  {
-    id: 12,
-    value: "",
-    checked: false,
-    title: "Interior Color",
-  },
-  {
-    id: 13,
-    value: "",
-    checked: false,
-    title: "Other Options",
-  },
-];
-function AccordionItem(props) {
+const AccordionItem = (props) => {
   const { isOpen, onToggle, data, onSearchChange, searchState } = props;
   const [showAll, setShowAll] = useState(false);
 
-  const filteredSearchState = searchState.filter((item) => item.id === data.id);
+  const filteredSearchState = searchState?.filter(
+    (item) => item.id === data.id
+  );
 
-  const handleInputChange = (event) => {
+  const handleInputChange = (event, subId) => {
     const { id, value, checked, type } = event.target;
     const newState = [...searchState];
+
     const index = newState.findIndex((item) => item.id === Number(id));
+
+    console.log(
+      "val",
+      value,
+      "check",
+      checked,
+      "type",
+      type,
+      "id",
+      id,
+      "subId",
+      subId,
+      "index",
+      index
+    );
 
     if (index !== -1) {
       if (type === "checkbox") {
-        newState[index].checked = checked;
+        newState[index].status[subId - 1].checked = checked;
       } else {
-        console.log("val");
+        // console.log("main val", newState[id - 1].status[subId - 1].value);
         newState[index].value = value;
-        console.log("valu", newState);
       }
-      onSearchChange(newState);
+      onSearchChange(newState, id, value, checked);
     }
   };
-  console.log(searchState);
 
+  // const handleInputChange = (event, id, subId) => {
+  //   const { id, value, checked, type } = event.target;
+  //   const newState = [...searchState];
+
+  //   const index = newState.findIndex((item) => item.id === Number(id));
+
+  //   if (index !== -1) {
+  //     if (type === "checkbox") {
+  //       // console.log("sd", newState[index].status , "all", searchState, "che" , checked);
+
+  //       newState[index].status = checked;
+  //     } else {
+  //       newState[index].value = value;
+  //     }
+  //     onSearchChange(newState, id, value, checked);
+  //   }
+  // };
   return (
     <div className="accordion">
       <div className="toggler" onClick={onToggle}>
@@ -279,8 +127,8 @@ function AccordionItem(props) {
                         name={list.name}
                         id={item.id}
                         value={list.name}
-                        checked={item.checked}
-                        onChange={handleInputChange}
+                        checked={list.checked}
+                        onChange={(e) => handleInputChange(e, list.id)}
                       />
                     </div>
                   ))}
@@ -305,8 +153,11 @@ function AccordionItem(props) {
                             type="checkbox"
                             name={list.name}
                             id={list.id}
-                            checked={item.checked}
-                            onChange={handleInputChange}
+                            value={list.name}
+                            checked={list.checked}
+                            onChange={(e) =>
+                              handleInputChange(e, item.id, list.id)
+                            }
                           />
                         </div>
                       ))}
@@ -320,7 +171,7 @@ function AccordionItem(props) {
       )}
     </div>
   );
-}
+};
 
 const SideNav = () => {
   const [searchState, setSearchState] = useState(initialSearchState);
@@ -341,12 +192,12 @@ const SideNav = () => {
   };
 
   // handle all search change
-  const handleSearchChange = (event, id) => {
-    const { value } = event.target;
+  const handleSearchChange = (event, id, value, checked) => {
+    // console.log("id", id, "val", value, "che", checked);
 
     setSearchState((prevState) => {
       // Find the index of the item to update
-      const index = prevState.findIndex((item) => item.id === id);
+      const index = prevState.findIndex((item) => item.id === Number(id));
 
       // Create a new array with the updated item
       const newState = [
@@ -354,9 +205,12 @@ const SideNav = () => {
         { ...prevState[index], value },
         ...prevState.slice(index + 1),
       ];
-
+      console.log(newState);
       return newState;
     });
+
+    // const { name, value } = event.target;
+    // setSearchState({ ...searchState, [name]: value });
   };
 
   return (
@@ -383,7 +237,7 @@ const SideNav = () => {
           data={item}
           onToggle={() => handleToggle(index)}
           onSearchChange={handleSearchChange}
-          searchState={initialSearchState}
+          searchState={searchState}
         />
       ))}
     </div>
